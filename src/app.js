f72eda8fbd086e441023625299ef127d4f5e4764
Monti <sergiomontalban@hotmail.es>
2021-03-11 21:52:12 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const app = express();
 
 const path = require('path');
 
+app.get("/", (req, res) => {
+    res.setHeader("set-cookie", ["setfromserver=1","cookiesegura=Valor seguro; Secure; httponly"])
+    res.sendFile(`${__dirname}`)
+});
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
